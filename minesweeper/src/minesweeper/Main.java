be69790a81155b21d07875b5ffdc42434f445353
Squigglevision <127502256+Squigglevision@users.noreply.github.com>
2023-10-23 23:01:57 +1100

    Fix cascade and clean up code

--- minesweeper/src/minesweeper/Main.java
+++ minesweeper/src/minesweeper/Main.java
@@ -1,20 +1,8 @@
 package minesweeper;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
-		
-		
-		// System objects - scanner & random
-		
-//		Scanner input = new Scanner(System.in);
-//		Random rand = new Random();
-//		
-		// game variables
-		
 		
         MineField minesweeper = new MineField();
         minesweeper.startGame();
--- minesweeper/src/minesweeper/MineField.java
+++ minesweeper/src/minesweeper/MineField.java
@@ -1,53 +1,52 @@
 package minesweeper;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class MineField {
 
-	private int size = 10;
-	private int rowLength =size;
-	private int columnLength = size;
-	private int bombCount = size;
+	private int rowLength = 10;
+	private int columnLength = 10;
+	private int bombCount = 10;
 	
 	private int bombNum = 100;
 	private int spaceNum = 50;
 	private int defaultNum = 0;
 	
-	private int[][] visibleBoard = new int[rowLength][columnLength];
-	private int[][] hiddenBoard = new int[rowLength][columnLength];
+	private int[][] visibleBoard;
+	private int[][] hiddenBoard;
+	Scanner scanner = new Scanner(System.in);
+
 	
 
 	
-	// Using colours in console:
-	//	public static final String BRIGHT_BLUE_TEXT = "\u001b-etc";
-	
-	// Have user able to play another game
-	// Fix cascade
-	
-	
 	public void startGame()
 	{
-		System.out.println("---- NEW MINESWEEPER GAME STARTED ----");
+		System.out.println("---- NEW MINESWEEPER GAME STARTED ----  \n");
+		visibleBoard = new int[rowLength][columnLength];
+		hiddenBoard = new int[rowLength][columnLength];
 		generateBombs();
 		setProximityNums();
-//		displayGameBoard(visibleBoard);
-		// NOTE: make sure to delete line below:
-		displayGameBoard(hiddenBoard);
+
 		
 		boolean playing = true;
 		while (playing)
 		{
 			displayGameBoard(visibleBoard);
-			playing = playMove();
-			if(checkWin())
-			{
-				displayGameBoard(hiddenBoard);
-				System.out.println("---- YOU WON THE GAME!! ----");
-				break;
-			}
-			
+			playing = playMove();	
 		}
+		
+        System.out.print("\n Would you like to play again? \n (y/n) \n ");
+        String playAgain= scanner.next();
+        if(playAgain.equals("y")) {
+        	System.out.println("... Starting new game ... \n");
+        	startGame();
+        } else {
+        	scanner.close();
+        	System.out.println("*** Thanks for playing! ***");
+        }      	
 	}
 	
 	public void generateBombs()
@@ -61,7 +60,6 @@ public class MineField {
 			int column = randomBomb.nextInt(columnLength);
 			// Add bomb if not already at coordinates
 			if(!(hiddenBoard[row][column] == bombNum)) {
-				System.out.println(" column: " +column + " row: " + row );
 				hiddenBoard[row][column] = bombNum;
 				fields++;
 			}	
@@ -160,12 +158,10 @@ public class MineField {
 	
 	public boolean playMove()
     {
-        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter Column Number: ");
         int column= scanner.nextInt();
         System.out.print("\nEnter Row Number: ");
         int row = scanner.nextInt();
-//        scanner.close();
 
         // Validate input:
         if(row < 0 || row > rowLength-1 || column < 0 || column > columnLength-1 || visibleBoard[row][column] != defaultNum)
@@ -178,12 +174,13 @@ public class MineField {
         if(hiddenBoard[row][column] == bombNum)
         {
         	displayGameBoard(hiddenBoard);
-            System.out.print("---- BOOM! You LOST the game! ----");
+            System.out.print("\n---- BOOM! You LOST the game! ---- \n");
             return false;
         }
-        else if(hiddenBoard[row][column] == spaceNum || (hiddenBoard[row][column] > 0 &&  hiddenBoard[row][column] < hiddenBoard.length - 1) )
+        else if(hiddenBoard[row][column] == spaceNum)
+
         {
-//            updateVisibleGameBoard(row, column);
+
         	showAdjacentSpacesAndNums(row, column);
         }
         else
@@ -191,251 +188,69 @@ public class MineField {
         	visibleBoard[row][column] = hiddenBoard[row][column];
         }
 
-        return true;
+        return checkWin();
     }
 	
+	
 	public void showAdjacentSpacesAndNums(int row, int column) {
-		int selectedSquare = hiddenBoard[row][column];
-		System.out.println(selectedSquare);
 		int selectedRowLength = 3;
 		int selectedColumnLength = 3;
-		int rowStartIndex = row;
-		int columnStartIndex = column;
+		int rowStartIndex = row-1;
+		int columnStartIndex = column-1;
 		
+		ArrayList<Coordinate> coordinates = new ArrayList<>(Arrays.asList());
 		
-		if(hiddenBoard[row][column] == spaceNum) {
-			for(int rowIndex = rowStartIndex; rowIndex < rowLength; rowIndex++) {
-				
-					if(hiddenBoard[rowIndex][column] != bombNum && rowIndex < rowLength-1 && rowIndex >= 0 && column < columnLength-1 && column >= 0) {
-						if(hiddenBoard[rowIndex][column] == spaceNum) {
-									visibleBoard[rowIndex][column] = hiddenBoard[rowIndex][column];
-									
-									for(int smallerColumn = column; smallerColumn > 0; smallerColumn--) {
-										if(smallerColumn > 0 && smallerColumn < columnLength && hiddenBoard[rowIndex][smallerColumn+1] == spaceNum) {
-											visibleBoard[rowIndex][smallerColumn] = hiddenBoard[rowIndex][smallerColumn];		
-										}
-
-									}
-									
-									for(int higherColumn = column; higherColumn < columnLength; higherColumn++) {
-										if(higherColumn > 0 && higherColumn < columnLength && hiddenBoard[rowIndex][higherColumn-1] == spaceNum) {
-											visibleBoard[rowIndex][higherColumn] = hiddenBoard[rowIndex][higherColumn];
-										}
-									}
-									
-
-							}
-						else {visibleBoard[rowIndex][column] = hiddenBoard[rowIndex][column];
-						break;};
-						
-					}	
-
-			}	
+		if(row == 0 || row == rowLength-1) {
+			selectedRowLength = 2;
+			if (row==0) {
+				rowStartIndex = 0;
+			}
 		}
 		
-		if(hiddenBoard[row][column] == spaceNum) {
-
-			for(int rowIndex = rowStartIndex; rowIndex >= 0; rowIndex--) {
-
-
-				
-					if(hiddenBoard[rowIndex][column] != bombNum && rowIndex >= 0 && column >= 0) {
-						if(hiddenBoard[rowIndex][column] == spaceNum) {
-									visibleBoard[rowIndex][column] = hiddenBoard[rowIndex][column];
-									
-									for(int smallerColumn = column; smallerColumn > 0; smallerColumn--) {
-										if(smallerColumn > 0 && smallerColumn < columnLength-1 && hiddenBoard[rowIndex][smallerColumn+1] == spaceNum) {
-											visibleBoard[rowIndex][smallerColumn] = hiddenBoard[rowIndex][smallerColumn];		
-										}
-
-									}
-									
-									for(int higherColumn = column; higherColumn < columnLength; higherColumn++) {
-										if(higherColumn > 0 && higherColumn < columnLength && hiddenBoard[rowIndex][higherColumn-1] == spaceNum) {
-											visibleBoard[rowIndex][higherColumn] = hiddenBoard[rowIndex][higherColumn];
-										}
-									}
-									
-
-							}
-						else {visibleBoard[rowIndex][column] = hiddenBoard[rowIndex][column];
-						break;};
-						
-					}	
-
-			}	
+		if(column == 0 || column == columnLength-1) {
+			selectedColumnLength = 2;
+			if(column == 0) {
+				columnStartIndex = 0;
+			}
 		}
 		
-		
-		if(hiddenBoard[row][column] == spaceNum) {
-			for(int columnIndex = columnStartIndex; columnIndex < columnLength; columnIndex++) {
+		for(int rowIndex = rowStartIndex; rowIndex < selectedRowLength + rowStartIndex; rowIndex++) {
+			for(int columnIndex = columnStartIndex; columnIndex < selectedColumnLength + columnStartIndex; columnIndex++) {
+				if(hiddenBoard[rowIndex][columnIndex] == spaceNum && visibleBoard[rowIndex][columnIndex] == defaultNum) {
+					coordinates.add(new Coordinate(rowIndex, columnIndex));
+				}
+				visibleBoard[rowIndex][columnIndex] = hiddenBoard[rowIndex][columnIndex];
 				
-					if(hiddenBoard[row][columnIndex] != bombNum && row < rowLength-1 && row >= 0 && columnIndex < columnLength-1 && columnIndex >= 0) {
-						if(hiddenBoard[row][columnIndex] == spaceNum) {
-									visibleBoard[row][columnIndex] = hiddenBoard[row][columnIndex];
-									
-						
-									
-									for(int smallerRow = row; smallerRow > 0; smallerRow--) {
-										if(smallerRow > 0 && smallerRow <rowLength-1 && hiddenBoard[smallerRow+1][columnIndex] == spaceNum) {
-											visibleBoard[smallerRow][columnIndex] = hiddenBoard[smallerRow][columnIndex];		
-										}
-
-									}
-									
-									for(int higherRow = row; higherRow < rowLength; higherRow++) {
-										if(higherRow < rowLength && higherRow > 0 && hiddenBoard[higherRow-1][columnIndex] == spaceNum) {
-											visibleBoard[higherRow][columnIndex] = hiddenBoard[higherRow][columnIndex];
-										}
-									}
-									
-
-							}
-						else {visibleBoard[row][columnIndex] = hiddenBoard[row][columnIndex];
-						break;
-						};
-						
-					}	
-
-			}	
+			}
+		}
+		for(int index = 0; index < coordinates.size(); index++) {
+			Coordinate coordinate = coordinates.get(index);
+			showAdjacentSpacesAndNums(coordinate.row, coordinate.column);
 		}
 		
-		
-		if(hiddenBoard[row][column] == spaceNum) {
-
-			for(int columnIndex = columnStartIndex; columnIndex >= 0; columnIndex--) {
-
-				
-					if(hiddenBoard[row][columnIndex] != bombNum && row >= 0 && columnIndex >= 0) {
-						if(hiddenBoard[row][columnIndex] == spaceNum) {
-									visibleBoard[row][columnIndex] = hiddenBoard[row][columnIndex];
-
-//									
-									for(int smallerRow = row; smallerRow > 0; smallerRow--) {
-										if(smallerRow > 0 && smallerRow < rowLength-1 && hiddenBoard[smallerRow+1][columnIndex] == spaceNum) {
-											visibleBoard[smallerRow][columnIndex] = hiddenBoard[smallerRow][columnIndex];		
-										}
-
-									}
-									
-									for(int higherRow = row; higherRow < rowLength; higherRow++) {
-										if(higherRow < rowLength && higherRow > 0 && hiddenBoard[higherRow-1][columnIndex] == spaceNum) {
-											visibleBoard[higherRow][columnIndex] = hiddenBoard[higherRow][columnIndex];
-										}
-									}
-
-							}
-						else {visibleBoard[row][columnIndex] = hiddenBoard[row][columnIndex];
-						break;};
-						
-					}	
-
-			}	
-		}
-		
-		
-		
-		
-		
-//		if(hiddenBoard[row][column] == spaceNum) {
-//			for(int rowIndex = rowStartIndex; rowIndex < rowLength; rowIndex++) {
-//				for(int columnIndex = columnStartIndex; columnIndex < columnLength; columnIndex++) {
-//					
-//					if(hiddenBoard[rowIndex][columnIndex] != bombNum && rowIndex < rowLength-1 && rowIndex >= 0 && columnIndex < columnLength-1 && columnIndex >= 0) {
-//						if(hiddenBoard[rowIndex][columnIndex] == spaceNum) {
-//									visibleBoard[rowIndex][columnIndex] = hiddenBoard[rowIndex][columnIndex];
-//									
-//
-//							}
-//						else visibleBoard[rowIndex][columnIndex] = hiddenBoard[rowIndex][columnIndex];
-//						
-//					}	
-//				}
-//			}	
-//		}
-//		
-//		
-//		if(hiddenBoard[row][column] == spaceNum) {
-//			for(int rowIndex = rowStartIndex; rowIndex > rowLength; rowIndex--) {
-//				for(int columnIndex = columnStartIndex; columnIndex > columnLength; columnIndex--) {
-//					
-//					if(hiddenBoard[rowIndex][columnIndex] != bombNum && rowIndex < rowLength-1 && rowIndex >= 0 && columnIndex < columnLength-1 && columnIndex >= 0) {
-//						if(hiddenBoard[rowIndex][columnIndex] == spaceNum) {
-//									visibleBoard[rowIndex][columnIndex] = hiddenBoard[rowIndex][columnIndex];
-//									
-//
-//							}
-//						else visibleBoard[rowIndex][columnIndex] = hiddenBoard[rowIndex][columnIndex];
-//						
-//					}	
-//				}
-//			}	
-//		}
-		
-		
-		else visibleBoard[row][column] = hiddenBoard[row][column];
-
 	}
-	
-	
-//	public void showAdjacentSpacesAndNums(int row, int column) {
-//		int selectedSquare = hiddenBoard[row][column];
-//		System.out.println(selectedSquare);
-//		int selectedRowLength = 3;
-//		int selectedColumnLength = 3;
-//		int rowStartIndex = row-1;
-//		int columnStartIndex = column-1;
-//		
-//		if(row == 0 || row == rowLength-1) {
-//			selectedRowLength = 2;
-//			if (row==0) {
-//				rowStartIndex = 0;
-//			}
-//		}
-//		
-//		if(column == 0 || column == columnLength-1) {
-//			selectedColumnLength = 2;
-//			if(column == 0) {
-//				columnStartIndex = 0;
-//			}
-//		}
-//		
-//		if(hiddenBoard[row][column] == spaceNum) {
-//			for(int rowIndex = rowStartIndex; rowIndex < selectedRowLength + rowStartIndex; rowIndex++) {
-//				for(int columnIndex = columnStartIndex; columnIndex < selectedColumnLength + columnStartIndex; columnIndex++) {
-//					
-//					if(hiddenBoard[rowIndex][columnIndex] != bombNum) {
-//						if(hiddenBoard[rowIndex][columnIndex] == spaceNum) {
-//									visibleBoard[rowIndex][columnIndex] = hiddenBoard[rowIndex][columnIndex];
-//									
-//
-//							}
-//						else visibleBoard[rowIndex][columnIndex] = hiddenBoard[rowIndex][columnIndex];
-//						
-//					}	
-//				}
-//			}	
-//		}
-//		else visibleBoard[row][column] = hiddenBoard[row][column];
-//
-//	}
     
     public boolean checkWin()
     {
-        for(int row = 0; row < 10; row++)
+        for(int row = 0; row < rowLength; row++)
         {
-            for(int column = 0; column < 10; column++)
+        	// Return (playing) true if there are any uncovered tiles:
+        	
+            for(int column = 0; column < columnLength; column++)
             {
-                if(visibleBoard[row][column]==0)
+                if(visibleBoard[row][column] == defaultNum)
                 {
                     if(hiddenBoard[row][column] != bombNum)
                     {
-                        return false;
+                        return true;
                     }
                 }
             }
         }
-        return true;
+        
+        displayGameBoard(hiddenBoard);
+		System.out.println("\n ---- YOU WON THE GAME!! ---- \n");
+        return false;
     }
 	
 	
